86964e99313e004e1ebfdbf0181c1fefb60bfc7b
zhuguoliang <zhuguoliangcs@163.com>
2018-10-29 16:38:02 +0800

    feat: add append component

--- src/component.rs
+++ src/component.rs
@@ -1,4 +1,3 @@
-#![feature(test)]
 //Component
 //组件是LSM树实现中的核心数据结构
 use std::fs::File;
@@ -7,43 +6,58 @@ use std::io;
 use test::Bencher;
 use helper;
 
+pub type DataT= Vec<Vec<u8>>;
 //size of u64
 const KEY_SIZE: usize = 8;
+const VALUE_SIZE:usize = 8;
+const FILENAME_SIZE:usize = 32; 
+const COMPONENT_NUM:usize= 6;
+const COMPONENT_SIZE:usize = 32;
 //align to make rust add padding so that every component will fit in cache
 #[repr(align(64))]
 #[derive(Debug)]
 pub struct Component{
-    pub keys:Vec<u8>,//int array
-    pub values:Vec<u8>,//int array
+    keys:DataT,//int array every key is vec<u8>
+    values:DataT,//every value is vec<u8>
     pub ne:usize,
     pub s:usize,//u64个数
     pub component_id:String
 }
-//key_size u64 value_size可变
+//key_size u64 (value_size is for each value vec<u8>)
 impl Component{
-    pub fn new(component_size:usize, value_size:usize, 
+    
+    pub fn new(component_size:usize, _value_size:usize, 
                 ne:usize, component_id:String) ->Component{
         Component{
             //keys:Vec::with_capacity(component_size*(mem::size_of::<u64> as usize)),
-            keys:vec![0u8,(component_size*KEY_SIZE) as u8],
-            values:Vec::with_capacity(component_size*value_size),
+            keys:Vec::with_capacity(ne),
+            values:Vec::with_capacity(ne),
             //values:vec![0u8,(component_size*(mem::size_of::<u64> as usize)) as u8],
             ne:ne,
             s:component_size,
             component_id:component_id
         }
     }
-    pub fn init(&mut self, component_size:usize, value_size:usize, 
+    pub fn init(&mut self, component_size:usize, _value_size:usize, 
                 ne:usize, component_id:&String) {
-            self.keys=Vec::with_capacity(component_size);
-            self.values=Vec::with_capacity(component_size*value_size);
+            self.keys=Vec::with_capacity(ne);
+            self.values=Vec::with_capacity(ne);
             self.ne=ne;
             self.s=component_size;
             self.component_id=component_id.to_owned();
     }
 
+    pub fn push_key(&mut self, key:Vec<u8>) {
+        assert!(key.len()==KEY_SIZE);//SIZE IN BYTES
+        self.keys.push(key);
+    }
+
+    pub fn push_value(&mut self, value:Vec<u8>) {
+        assert!(value.len()==VALUE_SIZE);
+        self.values.push(value);
+    }
     //TODO: this func can be made faster using char array?
-    pub fn create_disk_component(&mut self, name:String, nc:u64,filename_size:usize)
+    pub fn create_disk_component(&mut self, name:String, nc:usize,filename_size:usize)
                 ->io::Result<()>
     {
         let component_type:[String;2]=['k'.to_string(),'v'.to_string()];
@@ -76,16 +90,22 @@ impl Component{
         let filename_values=helper::get_files_name(&name,&component_id,"v",filename_size);
 
         helper::load_file_to_vec(&mut self.keys, &filename_keys, KEY_SIZE, ne);
-        helper::load_file_to_vec(&mut self.values, &filename_values, value_size, ne);
+        helper::load_file_to_vec(&mut self.values, &filename_values, VALUE_SIZE, ne);
 
+        //assert!(ne<=self.values.len());
+        //assert!(ne<=self.keys.len());
     }
 
 //value_size:usize is unnecessary because rust has write_all
-    pub fn write_disk_component(&mut self, name:String,
+    pub fn write_disk_component(&mut self, name:String, ne:usize,
                             filename_size:usize){
 
         let filename_keys=helper::get_files_name(&name,&self.component_id,"k",filename_size);
         let filename_values=helper::get_files_name(&name,&self.component_id,"v",filename_size);
+
+        assert!(ne<=self.values.len());
+        assert!(ne<=self.keys.len());
+
         helper::flush_vec_to_file(&mut self.keys, &filename_keys);
         helper::flush_vec_to_file(&mut self.values, &filename_values);
     }
@@ -93,15 +113,14 @@ impl Component{
 // Append to a component on disk the last n keys/values 
     pub fn append_on_disk(&mut self, n:usize,name:String,
     value_size:usize,filename_size:usize) {
+        assert!(value_size==VALUE_SIZE);
         let filename_keys=helper::get_files_name(&name,&self.component_id,"k",filename_size);
         let filename_values=helper::get_files_name(&name,&self.component_id,"v",filename_size);
         helper::append_last_n_to_file(&mut self.keys, &filename_keys, n);
         helper::append_last_n_to_file(&mut self.values, &filename_values, n);
     }
+
 }
-
-
-
 
 #[test]
 fn test_init(){
@@ -120,12 +139,13 @@ fn test_init(){
 #[test]
 fn test_create_disk_component()
 {
-    let mut c:Component = Component::new(10, 10, 2, 3.to_string());
-    let _created = match c.create_disk_component("hello".to_string(), 10, 32){
+    let component_size=10;
+    let ne=2;
+    let mut c:Component = Component::new(component_size, VALUE_SIZE, ne, 3.to_string());
+    let _created = match c.create_disk_component("hello".to_string(), COMPONENT_NUM, FILENAME_SIZE){
         Err(why)=> panic!("could not create due to  {}", why),
         Ok(())=> println!("Succefully created component on disk"), 
     };
-    //assert_eq!(c.create_disk_component("hello".to_string(), 10, 30),);
 
 }
 
@@ -133,33 +153,83 @@ fn test_create_disk_component()
 #[test]
 fn test_write_component()
 {
-    let mut c:Component = Component::new(10, 10, 2, 3.to_string());
-    let _createres = match c.create_disk_component("hello".to_string(), 10, 30){
+    let component_size=10;
+    let ne=2;
+    let mut c:Component = Component::new(component_size, VALUE_SIZE, ne, 3.to_string());
+    //let mut c:Component = Component::new(10, 10, 2, 3.to_string());
+    let _createres = match c.create_disk_component("hello".to_string(), COMPONENT_NUM, FILENAME_SIZE){
         Err(why)=> panic!("could not open due to  {}", why),
         Ok(())=> println!("Succefully created component on disk"),
     };
-    c.keys.insert(0, 100);
-    c.values.insert(0, 101);
-    c.write_disk_component("hello".to_string(), 20);
+
+    for _entry in 0..ne{
+        let mut key = Vec::new();
+        let mut value = Vec::new();
+        for i in 100..(100+KEY_SIZE) {
+            key.push(i as u8);
+        }
+        for j in 200..(200+VALUE_SIZE) {
+            value.push(j as u8);
+        }
+        c.push_key(key);
+        c.push_value(value);
+    }
+    c.write_disk_component("hello".to_string(), ne,FILENAME_SIZE);
 }
 
 
 #[test]
 fn test_read_component()
 {
-    let mut c:Component = Component::new(10, 10, 2, 3.to_string());
-    let _createres = match c.create_disk_component("hello".to_string(), 10, 30){
+    let ne=2;
+    let mut c:Component = Component::new(COMPONENT_SIZE, VALUE_SIZE, ne, 3.to_string());
+    let _createres = match c.create_disk_component("hello".to_string(), COMPONENT_NUM, FILENAME_SIZE){
         Err(why)=> panic!("could not open due to  {}", why),
         Ok(())=> println!("Succefully created component on disk"),
     };
-    c.keys.insert(0, 100);
-    c.values.insert(0, 101);
-    c.write_disk_component("hello".to_string(), 20);
-    //c.read_disk_component("hello".to_string(), 20);
-    c.read_disk_component("hello".to_string(), 10, 3.to_string(), 10, 10, 20);
+
+    for _entry in 0..ne{
+        let mut key = Vec::new();
+        let mut value = Vec::new();
+        for i in 100..(100+KEY_SIZE) {
+            key.push(i as u8);
+        }
+        for j in 200..(200+VALUE_SIZE) {
+            value.push(j as u8);
+        }
+        c.push_key(key);
+        c.push_value(value);
+    }
+    c.write_disk_component("hello".to_string(), ne,FILENAME_SIZE);
+    c.read_disk_component("hello".to_string(), ne, 3.to_string(), COMPONENT_SIZE, VALUE_SIZE, FILENAME_SIZE);
     
 }
 
+#[test]
+fn test_append_n_kvpair()
+{
+    let ne=2;
+    let mut c:Component = Component::new(COMPONENT_SIZE, VALUE_SIZE, ne, 3.to_string());
+    let _createres = match c.create_disk_component("hello".to_string(), COMPONENT_NUM, FILENAME_SIZE){
+        Err(why)=> panic!("could not open due to  {}", why),
+        Ok(())=> println!("Succefully created component on disk"),
+    };
+    for _entry in 0..ne{
+        let mut key = Vec::new();
+        let mut value = Vec::new();
+        for i in 100..(100+KEY_SIZE) {
+            key.push(i as u8);
+        }
+        for j in 200..(200+VALUE_SIZE) {
+            value.push(j as u8);
+        }
+        c.push_key(key);
+        c.push_value(value);
+    } 
+    c.write_disk_component("hello".to_string(), ne,FILENAME_SIZE);
+    c.read_disk_component("hello".to_string(), ne, 3.to_string(), COMPONENT_SIZE, VALUE_SIZE, FILENAME_SIZE);
+    c.append_on_disk(2, "hello".to_string(), VALUE_SIZE, FILENAME_SIZE);
+}
 
 #[bench]
 fn empty(b: &mut Bencher) {
--- src/helper.rs
+++ src/helper.rs
@@ -1,8 +1,8 @@
 //This is helper function for this project
 use std::io::prelude::*;
 use std::fs::OpenOptions;
-use std::io::{Read};
-
+use std::io::{Read,BufReader,BufWriter};
+//use std::fs::File;
 pub fn get_files_name(name:&String, component_id:&String, 
     component_type:&str, filename_size:usize) ->String {
     let mut filename = String::with_capacity(filename_size + 8);
@@ -15,22 +15,67 @@ pub fn get_files_name(name:&String, component_id:&String,
     filename
 }
 
-pub fn load_file_to_vec(resvec:&mut Vec<u8>, fname:&String, one_size:usize,
+//one key/value a line
+pub fn load_file_to_vec(resvec:&mut Vec<Vec<u8>>, fname:&String, one_size:usize,
         ne:usize){
-  
+
+    println!("load filename is {}",fname );
     let f = match OpenOptions::new().read(true).open(fname) {
                 Err(why)=> panic!("could not open due to  {}", why),
                 Ok(fkeys)=> fkeys,
             };
 
+    let file = BufReader::new(&f);
+
+    let mut rawvec:Vec<u8> = Vec::with_capacity(one_size*ne);
+    //take unit is bytes
+    match file.take((one_size * ne)as u64).read_to_end(&mut rawvec) {
+       Err(why)=> panic!("cannot read from file due to {}",why),
+       Ok(readsize)=> println!("Succefully read {} bytes", readsize ),
+    };
+    for i in 0..ne {
+        resvec.push(rawvec[i*one_size..(i*one_size+one_size)].to_vec());
+        //println!("i is {}",i );
+    }
+}
+
+pub fn load_file_to_vec1(resvec:&mut Vec<u8>, fname:&String, one_size:usize,
+        ne:usize){
+    let f = match OpenOptions::new().read(true).open(fname) {
+                Err(why)=> panic!("could not open due to  {}", why),
+                Ok(fkeys)=> fkeys,
+            };
+    let f =BufReader::new(f);
+
     match f.take((one_size*ne) as u64).read_to_end(resvec) {
         Err(why)=> panic!("cannot read from file due to {}",why),
         Ok(readsize)=> println!("Succefully read {} bytes", readsize ),
-    };
+    }; 
 
+    //match  
 }
 
-pub fn flush_vec_to_file(u8vec:&mut Vec<u8>, fname:&String)
+
+pub fn flush_vec_to_file(u8vec:&mut Vec<Vec<u8>>, fname:&String)
+{
+       //using scope to control file open range
+    //Reading files
+    let f = match OpenOptions::new().append(true).open(&fname) {
+        Err(why)=> panic!("could not open due to  {}", why),
+        Ok(f)=> f,
+    };
+    println!("flush filename is {}",fname );
+    let mut f=BufWriter::new(f); 
+
+    for lvec in u8vec {
+        match f.write_all(&lvec){
+            Err(why)=> panic!("Could not write due to {}",why),
+            Ok(_writtensize)=> (),
+        }
+    }
+}
+
+pub fn flush_vec_to_file0(u8vec:&mut Vec<u8>, fname:&String)
 {
     //using scope to control file open range
     //Reading files
@@ -44,20 +89,67 @@ pub fn flush_vec_to_file(u8vec:&mut Vec<u8>, fname:&String)
         Ok(())=> println!("Succefully written to file {}",fname),
     }
 }
-
-pub fn append_last_n_to_file(u8vec:&mut Vec<u8>, fname:&String,n:usize)
+//buffered version
+pub fn flush_vec_to_file1(u8vec:&mut Vec<u8>, fname:&String)
 {
-    assert!(n>=u8vec.len(),"There is no more than {} element in component",n);
-    
-    let mut f = match OpenOptions::new().write(true).append(true).open(&fname) {
+    //using scope to control file open range
+    //Reading files
+    let f = match OpenOptions::new().write(true).open(&fname) {
         Err(why)=> panic!("could not open due to  {}", why),
         Ok(f)=> f,
     };
-
-    let target=u8vec[u8vec.len()-n .. u8vec.len()].to_vec();
-    match f.write_all(&target) {
+    let mut f=BufWriter::new(f);
+    match f.write_all(&u8vec) {
         Err(why)=> panic!("cannot write to file due to {}",why),
         Ok(())=> println!("Succefully written to file {}",fname),
-    }
+    } 
+     match f.flush() {
+        Err(why)=> panic!("cannot flush to file due to {}",why),
+        Ok(())=> println!("Succefully flush to file {}",fname),
+    } 
 
 }
+
+pub fn append_last_n_to_file(u8vec:&mut Vec<Vec<u8>>, fname:&String,n:usize)
+{
+    assert!(n>=u8vec.len(),"There is no more than {} element in component",n);
+    let f = match OpenOptions::new().append(true).open(&fname) {
+        Err(why)=> panic!("could not open due to  {}", why),
+        Ok(f)=> f,
+    };
+    let mut f=BufWriter::new(f);
+    for i in u8vec.len()-n..u8vec.len() {
+        match f.write_all(&u8vec[i]){
+            Err(why)=> panic!("Could not write due to {}",why),
+            Ok(_writtensize)=> (),
+        }
+    } 
+}
+
+// fn transform_u32_to_array_of_u8(x:u32) -> [u8;4] {
+//     let b1 : u8 = ((x >> 24) & 0xff) as u8;
+//     let b2 : u8 = ((x >> 16) & 0xff) as u8;
+//     let b3 : u8 = ((x >> 8) & 0xff) as u8;
+//     let b4 : u8 = (x & 0xff) as u8;
+//     return [b1, b2, b3, b4]
+// }
+
+
+// fn transform_u64_to_array_of_u8(x:u64) -> [u8;8] {
+//     let b1 : u8 = ((x >> 56) & 0xff) as u8;
+//     let b2 : u8 = ((x >> 48) & 0xff) as u8;
+//     let b3 : u8 = ((x >> 40) & 0xff) as u8;
+//     let b4 : u8 = ((x >> 32) & 0xff) as u8;
+//     let b5 : u8 = ((x >> 24) & 0xff) as u8;
+//     let b6 : u8 = ((x >> 16) & 0xff) as u8;
+//     let b7 : u8 = ((x >> 8) & 0xff) as u8;
+//     let b8 : u8 = (x & 0xff) as u8; 
+//     return [b1, b2, b3, b4, b5, b6, b7, b8]
+// }
+
+// fn transform_array_of_u8_to_u64(u8vec:Vec<u8>) -> u64 {
+//     (u8vec[0] as u64) << 56 |(u8vec[1] as u64) << 48|
+//     (u8vec[2] as u64) << 40 |(u8vec[3] as u64) << 32|
+//     (u8vec[4] as u64) << 24 |(u8vec[5] as u64) << 16|
+//     (u8vec[6] as u64) <<  8 |(u8vec[7] as u64)
+// }
